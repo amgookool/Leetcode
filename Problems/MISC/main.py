b0ab382a3875d1124adf409fa1b37a78c8d62aa8
Adrian Gookool <amgookool@hotmail.com>
2024-05-28 22:39:30 -0400

    Removed MISC

--- Problems/MISC/main.py
+++
@@ -1,7 +0,0 @@
-from typing import List
-def removeElement(nums: List[int] =[3,2,2,3] , val: int = 3) -> int:
-    nums[:] = [num for num in nums if num != val]
-    return len(nums)
-
-if __name__ == '__main__':
-    removeElement()
